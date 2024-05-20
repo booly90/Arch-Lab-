c7a75a155fb8d393fbc99700245efcc55aa691e2
booly90 <ylevy96@gmail.com>
2024-05-20 15:53:26 +0300

    Shifter Cout added
    added Cout for shifter

--- lab1 - submission/Shifter.vhd
+++ lab1 - submission/Shifter.vhd
@@ -9,7 +9,7 @@ ENTITY Shifter IS
 			k: INTEGER :=3 ; --k=log2(n), default 3
 			 );
 	PORT (  y_Shifter_in: IN std_logic_vector (n-1 downto 0);
-			x_Shifter_in: IN std_logic_vector (k-1 downto 0);
+			x_Shifter_in: IN std_logic_vector (n-1 downto 0);
 			ALUFN : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
 			Shifter_cout: OUT std_logic;
 			Shifter_out: OUT std_logic_vector (n-1 downto 0)
@@ -27,23 +27,23 @@ ARCHITECTURE dataflow of Shifter IS
 	SIGNAL row_left: matrix;
 	SIGNAL row_rigth: matrix;
 BEGIN
-	row_left(0) <= y_Shifter_in ;
-	SHL: for i IN 1 to n-1-1 generate
+	row_left(0)  <= y_Shifter_in ;
+	row_right(0) <= y_Shifter_in ;
+	SHL: for i IN 1 to n-1 generate
 		row_left(i) <= row_left(i-1)(n-2 downto 0) & '0'; --shift previous row_left 1 bit to the left and insert '0'
 		Shifter_cout_vector_left(i-1) <= row_left(i-1)(n-1); --cout recieves the last bit that has been pushed out
 	END generate
 	
-	row_right(0) <= y_Shifter_in ;
 	SLR: for i IN 1 to n-1 generate
 		row_right(i) <= '0' & row_right(i-1)(n-1 downto 1); --shift previous row_right 1 bit to the right and insert '0'
 		Shifter_cout_vector_right(i-1) <= row_left(i-1)(0); --cout recieves the last bit that has been pushed out
 	END generate
 	
 	
-	Shifter_out <= row_left(x_Shifter_in)  when ALUFN = "000" else
-				   row_right(x_Shifter_in) when ALUFN = "001" else (others => '0');
+	Shifter_out <=  row_left(x_Shifter_in(k-1 DOWNTO 0))  when ALUFN = "000" else  --choose output based on ALUFN signals
+				   row_right(x_Shifter_in(k-1 DOWNTO 0)) when ALUFN = "001" else (others => '0');
 				   
-	Shifter_cout <= Shifter_cout_vector_left(x_Shifter_in) when ALUFN = "000" else
-				   Shifter_cout_vector_right(x_Shifter_in) when ALUFN = "001" else '0';
+	Shifter_cout <= Shifter_cout_vector_left(x_Shifter_in(k-1 DOWNTO 0)) when ALUFN = "000" else -- choose cout based on ALUFN signals
+				   Shifter_cout_vector_right(x_Shifter_in(k-1 DOWNTO 0)) when ALUFN = "001" else '0';
 	
 END Shifter
