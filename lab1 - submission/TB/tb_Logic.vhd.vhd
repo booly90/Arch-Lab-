4d037a8f5805985a1cc2befe033662090f2cd9b5
booly90 <ylevy96@gmail.com>
2024-05-22 17:07:03 +0300

    1
    1

--- lab1 - submission/TB/tb_Logic.vhd.vhd
+++ lab1 - submission/TB/tb_Logic.vhd.vhd
@@ -13,39 +13,37 @@ end tb_Logic;
 ------------------------------------------------------------------------------
 architecture rtb of tb_Logic is
 
-   component Logic is
+   COMPONENT Logic IS 
 	GENERIC (
-        CONSTANT n : INTEGER := 8;  -- Example constant, typically set to your desired value
-        CONSTANT k : INTEGER := 3;  -- log2(n), here assumed to be 3
-        CONSTANT m : INTEGER := 4   -- 2^(k-1), here assumed to be 4
-    );
-    PORT (
-        Y_Logic_i: in  std_logic_vector(n-1 DOWNTO 0);
-        X_Logic_i: in  std_logic_vector(n-1 DOWNTO 0);
-        ALUFN: in STD_LOGIC_VECTOR (k-1 downto 0);            --- ALUFN[2:0]
-        Logic_o: out std_logic_vector(n-1 DOWNTO 0)
-    );
-	end component;
+				n: INTEGER :=8;   --bit size of input and outputs signals, default is 8
+				OP : INTEGER :=8  --number of logical operations plus 1 (for  zeros), default is 8
+				);
+	PORT(
+			x_logic,y_logic : IN std_logic_vector (n-1 Downto 0);
+			ALUFN : IN std_logic_vector (2 Downto 0);
+			Logic_out : OUT std_logic_vector (n-1 Downto 0)
+		);
+END Logic
 	
 	
 
-	signal Y_Logic_i, X_Logic_i, Logic_o: std_logic_vector(n-1 DOWNTO 0);
+	signal y_logic, x_logic, Logic_out: std_logic_vector(n-1 DOWNTO 0);
     signal ALUFN: std_logic_vector(k-1 DOWNTO 0);      
     begin
-	L0 : Logic generic map (n,k,m) port map(Y_Logic_i, X_Logic_i, ALUFN, Logic_o);
+	L0 : Logic generic map (n,k,m) port map(y_logic, x_logic, ALUFN, Logic_out);
     
 	--------- start of stimulus section ---------------------------------------		
-        tb_x_y : process
+        tb_xy_inputs : process
         begin
             ------- NOT testing -------
         
-		  X_Logic_i <= (others => '0');
-		  Y_Logic_i <= (others => '1');
+		  x_logic <= (others => '0');
+		  y_logic <= (others => '1');
           for j in 0 to 7 loop
             for k in 0 to 3 loop
 		        wait for 50 ns;   ---- Output: "00000000"
-                 Y_Logic_i <= Y_Logic_i + 3;
-                 X_Logic_i <= X_Logic_i + 2;
+                 y_logic <= y_logic + 3;
+                 x_logic <= x_logic + 2;
                 end loop;
           end loop;      
         wait;
