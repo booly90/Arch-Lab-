52379cfae41d126b14895206b70c18fd54f9f93a
booly90 <ylevy96@gmail.com>
2024-05-22 19:45:10 +0300

    #2 rev of top
    finished cleaning syntax errors from the top module, ready for TB testing

--- lab1 - submission/DUT/top.vhd
+++ lab1 - submission/DUT/top.vhd
@@ -2,7 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
-USE work.aux_package.all;
+USE wORk.aux_package.all;
 -------------------------------------
 ENTITY top IS
   GENERIC (n : INTEGER := 8;
@@ -18,23 +18,23 @@ ENTITY top IS
 END top;
 ------------- complete the top Architecture code --------------
 ARCHITECTURE struct OF top IS 
-	subtype vector IS STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+	subtype vectOR IS STD_LOGIC_VECTOR(n-1 DOWNTO 0);
 
 	--modules input wires
-	SIGNAL x_adderSub_in: vector;
-	SIGNAL x_Logic_in: vector;
-	SIGNAL x_Shifter_in: vector;
-	SIGNAL y_adderSub_in: vector;
-	SIGNAL y_Logic_in: vector;
-	SIGNAL y_Shifter_in: vector;
+	SIGNAL x_adderSub_in: vectOR;
+	SIGNAL x_Logic_in: vectOR;
+	SIGNAL x_Shifter_in: vectOR;
+	SIGNAL y_adderSub_in: vectOR;
+	SIGNAL y_Logic_in: vectOR;
+	SIGNAL y_Shifter_in: vectOR;
 	
 	--modules output wires
-	SIGNAL adderSub_out: vector;
-	SIGNAL Shifter_out: vector;
-	SIGNAL Logic_out: vector;
-	SIGNAL adderSub_cout: vector;
-	SIGNAL Shifter_cout: vector;
-	SIGNAL Logic_cout: vector;
+	SIGNAL adderSub_out: vectOR;
+	SIGNAL Shifter_out: vectOR;
+	SIGNAL Logic_out: vectOR;
+	SIGNAL adderSub_cout: STD_LOGIC;
+	SIGNAL Shifter_cout:  STD_LOGIC;
+	SIGNAL Logic_cout:    STD_LOGIC;
 	
 	
 	
@@ -46,67 +46,73 @@ ARCHITECTURE struct OF top IS
 BEGIN
 	
  --gate inputs to AdderSub module when active
-	y_adderSub_in <= Y_i when ALUFN_i(4 DOWNTO 3) = "01" else (others => '0')
-	x_adderSub_in <= X_i when ALUFN_i(4 DOWNTO 3) = "01" else (others => '0')
+	y_adderSub_in <= Y_i when ALUFN_i(4 DOWNTO 3) = "01" else (others => '0');
+	x_adderSub_in <= X_i when ALUFN_i(4 DOWNTO 3) = "01" else (others => '0');
 	
  --gate inputs to Logic module when active
-	y_Logic_in <= Y_i when ALUFN_i(4 DOWNTO 3) = "11" else (others => '0')
-	x_Logic_in <= X_i when ALUFN_i(4 DOWNTO 3) = "11" else (others => '0')
+	y_Logic_in    <= Y_i when ALUFN_i(4 DOWNTO 3) = "11" else (others => '0');
+	x_Logic_in    <= X_i when ALUFN_i(4 DOWNTO 3) = "11" else (others => '0');
 	
  --gate inputs to Shifter module when active
-	y_Shifter_in <= Y_i when ALUFN_i(4 DOWNTO 3) = "10" else (others => '0')
-	x_Shifter_in <= X_i when ALUFN_i(4 DOWNTO 3) = "10" else (others => '0')
+	y_Shifter_in  <= Y_i when ALUFN_i(4 DOWNTO 3) = "10" else (others => '0');
+	x_Shifter_in  <= X_i when ALUFN_i(4 DOWNTO 3) = "10" else (others => '0');
 	
 	
 --determine if V flag should be raised
-	Vflag_add <= '1' WHEN -- pos+pos=neg or neg+neg=pos
-	( ALUFN_i = "01000" and ((not y_adderSub_in(n-1) and not x_adderSub_in(n-1) and     adderSub_out(n-1)) 
-					   or ((    y_adderSub_in(n-1) and     x_adderSub_in(n-1) and not adderSub_out(n-1)))))
-	or -- pos-neg=neg or neg-pos=pos
-	( ALUFN_i = "01001" and ((not y_adderSub_in(n-1) and     x_adderSub_in(n-1) and     adderSub_out(n-1)) 
-					   or ((    y_adderSub_in(n-1) and not x_adderSub_in(n-1) and not adderSub_out(n-1)))))
-	else '0'; --when not overflow or when not arithmetic op
+	Vflag_add <= '1' WHEN -- pos+pos=neg OR neg+neg=pos otherwise set to 0
+					((not y_adderSub_in(n-1)  AND not x_adderSub_in(n-1) AND     adderSub_out(n-1))  = '1' 
+				OR  ((     y_adderSub_in(n-1) AND     x_adderSub_in(n-1) AND not adderSub_out(n-1))) = '1')
+				else '0';
+				
+	Vflag_sub <= '1' WHEN  -- pos-neg=neg OR neg-pos=pos otherwise set to 0
+					((not y_adderSub_in(n-1) AND     x_adderSub_in(n-1) AND     adderSub_out(n-1))  = '1' 
+				OR  ((    y_adderSub_in(n-1) AND not x_adderSub_in(n-1) AND not adderSub_out(n-1))) = '1')
+				else '0';
+				
 	
 	
 --output logic section
 --==========================================================--
-	Vflag_o <= Vflag_add or Vflag_sub; --note that for illegal ALUFN is set to 0
-	
-	Nflag_o <= 	adderSub_out(n-1) when ALUFN_i(4 DOWNTO 3) = "01" else
-				Shifter_out(n-1)  when ALUFN_i(4 DOWNTO 3) = "10" else
-				Logic_out(n-1)    when ALUFN_i(4 DOWNTO 3) = "11" 
-				else '0';
-				
-	Zflag_o <= '1' when (ALUFN_i(4 DOWNTO 3) = "01" or 
-						 ALUFN_i(4 DOWNTO 3) = "10" or 
-	                     ALUFN_i(4 DOWNTO 3) = "11") and
-						 ALUout_o = ((n-1 DOWNTO 0 => '0')
-				else '0';
-	
-	Cflag_o <=  adderSub_cout when ALUFN_i(4 DOWNTO 3) = "01" else
-				Shifter_cout  when ALUFN_i(4 DOWNTO 3) = "10" else
-				else '0'; --logic ops do not affect carry flag
-	
 	ALUout_o <= adderSub_out when ALUFN_i(4 DOWNTO 3) = "01" else
 				Shifter_out  when ALUFN_i(4 DOWNTO 3) = "10" else
 				Logic_out    when ALUFN_i(4 DOWNTO 3) = "11" 
+				else (others => '0');
+
+	Vflag_o <= 	Vflag_add when ALUFN_i = "01000" else 
+				Vflag_sub when ALUFN_i = "01001" 
 				else '0';
+	
+	Nflag_o <= 	adderSub_out(n-1) when ALUFN_i(4 DOWNTO 3) = "01" else
+				Shifter_out (n-1) when ALUFN_i(4 DOWNTO 3) = "10" else
+				Logic_out   (n-1) when ALUFN_i(4 DOWNTO 3) = "11" 
+				else '0';
+				
+	Zflag_o <= '1' when ((ALUFN_i(4 DOWNTO 3) = "01" and adderSub_out = (n-1 DOWNTO 0 => '0')) OR 
+						 (ALUFN_i(4 DOWNTO 3) = "10" and Shifter_out  = (n-1 DOWNTO 0 => '0')) OR 
+	                     (ALUFN_i(4 DOWNTO 3) = "11" and Logic_out    = (n-1 DOWNTO 0 => '0')))
+				else '0';
+	
+	Cflag_o <=  adderSub_cout when ALUFN_i(4 DOWNTO 3) = "01" else
+				Shifter_cout  when ALUFN_i(4 DOWNTO 3) = "10" 
+				else '0'; --logic ops do not affect carry flag
+	
+	
 	
 --==========================================================--
 
 		
 --component instantiation section
 --==========================================================--
-	AdderSub_inst: AdderSub GENERIC map(n => n);
+	AdderSub_inst: AdderSub GENERIC map(n => n)
 		PORT map(
 				x_adderSub_in => x_adderSub_in,
-				x_adderSub_in => y_adderSub_in,
+				y_adderSub_in => y_adderSub_in,
 				ALUFN 		  => ALUFN_i(2 DOWNTO 0),
 				adderSub_cout => adderSub_cout,
 				adderSub_out  => adderSub_out
 	);
 				
-	Logic_inst: Logic GENERIC map(n => n);
+	Logic_inst: Logic GENERIC map(n => n)
 		PORT map(
 				x_logic    => x_Logic_in,
 				y_logic    => y_Logic_in,
@@ -114,10 +120,10 @@ BEGIN
 				Logic_out  => Logic_out
 	);
 	
-	AdderSub_inst: AdderSub GENERIC map(n => n , k => k);
+	Shifter_inst: Shifter GENERIC map(n => n , k => k)
 		PORT map(
 				x_Shifter_in => x_adderSub_in,
-				x_Shifter_in => y_adderSub_in,
+				y_Shifter_in => y_adderSub_in,
 				ALUFN 	     => ALUFN_i(2 DOWNTO 0),
 				Shifter_cout => Shifter_cout,
 				Shifter_out  => Shifter_out
