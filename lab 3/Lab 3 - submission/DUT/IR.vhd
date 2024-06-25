42d9efa37e158eefb73d50820eb01f5653fb033a
Hadardoronn <hadardoron5@gmail.com>
2024-06-25 22:25:04 +0300

    dd

--- lab 3/Lab 3 - submission/DUT/IR.vhd
+++ lab 3/Lab 3 - submission/DUT/IR.vhd
@@ -1,54 +1,59 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.std_logic_unsigned.all;
-USE work.aux_package.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;  -- For std_logic_vector arithmetic
+use work.aux_package.all;
 
-----------------------------------------
-entity IR is 
-    generic(
-        bussize      : integer := 16;
-        RFAdWidth    : integer := 4;
-        OffsetSize   : integer := 8;
-        immSize_1    : integer := 4;
-		immSize_2    : integer := 8
-    );
-    port(
-        PMdataOut      : in  std_logic_vector(bussize-1 downto 0);
-        IRin           : in  std_logic;
-        RFaddr         : in  std_logic_vector(1 downto 0);
-        RWAddrRF       : out std_logic_vector(RFAdWidth-1 downto 0);
-        OffsetAddr     : out std_logic_vector(OffsetSize-1 downto 0);
-        Immid_1        : out std_logic_vector(immSize_1-1 downto 0);
-		Immid_2        : out std_logic_vector(immSize_2-1 downto 0);
-        IR_op          : out std_logic_vector(RFAdWidth-1 downto 0)
+entity IRunit is
+  generic (
+    bus_size : integer := 16
+  );
+  port (
+    bus_in : in std_logic_vector(bus_size-1 downto 0);
+    IRin : in std_logic;
+    RFaddr : in std_logic_vector(1 downto 0);
+    reg_address : out std_logic_vector(3 downto 0);
+    Op : out std_logic_vector(3 downto 0);
+    Imm1 : out std_logic_vector(bus_size-1 downto 0);
+    Imm2 : out std_logic_vector(bus_size-1 downto 0);
+    offset_addr : out std_logic_vector(7 downto 0)
+  );
+end IRunit;
 
-    );
-end IR;
+architecture behavioral of IRunit is
+  signal full_bus_in : std_logic_vector(bus_size-1 downto 0);
+  
+  alias opcode is full_bus_in(bus_size-1 downto bus_size-4);
+  alias ra is full_bus_in(bus_size-5 downto bus_size-8);
+  alias rb is full_bus_in(bus_size-9 downto bus_size-12);
+  alias rc is full_bus_in(bus_size-13 downto 0);
+  alias IR_8 is full_bus_in(7 downto 0);
+  
+begin
+  process (IRin, bus_in)
+  begin
+    if IRin = '1' then
+      full_bus_in <= bus_in;
+    end if;
+  end process;
 
------------------------------------------
-ARCHITECTURE dlf OF IR IS
-    signal IR_reg : std_logic_vector(bussize-1 downto 0) := (others => '0');
+  process (RFaddr, ra, rb, rc)
+  begin
+    case RFaddr is
+      when "01" => reg_address <= ra;
+      when "10" => reg_address <= rb;
+      when "11" => reg_address <= rc;
+      when others => reg_address <= (others => '0');
+    end case;
+  end process;
 
-begin 
-    process(IRin, PMdataOut)
-    begin
-        if IRin = '1' then
-            IR_reg <= PMdataOut;
-        end if;
-    end process;
-
-    IR_op <= IR_reg(4 * RFAdWidth - 1 downto 3 * RFAdWidth); --we put only 4 msb in IR
-
-    with RFaddr select
-        RWAddrRF <= IR_reg(RFAdWidth-1 downto 0) when "00",  -- R[c]
-                    IR_reg(2 * RFAdWidth - 1 downto RFAdWidth) when "01",  -- R[b]
-                    IR_reg(3 * RFAdWidth - 1 downto 2 * RFAdWidth) when others;  -- R[a]
-
-    OffsetAddr <= IR_reg(OffsetSize-1 downto 0);
-
-    Immid_1 <= IR_reg(immSize_1-1 downto 0);
-	Immid_2 <= IR_reg(immSize_2-1 downto 0);
-
-end dlf;
-	
-	
+  process (IRin, opcode, IR_8, rc)
+  begin
+    if IRin = '1' then
+      Op <= opcode;
+      Imm1 <= std_logic_vector(resize(signed(IR_8), Imm1'length));
+      Imm2 <= std_logic_vector(resize(signed(rc), Imm2'length));
+      offset_addr <= IR_8;
+    end if;
+  end process;
+  
+end behavioral;
--- lab 3/Lab 3 - submission/DUT/aux_package.vhd
+++ lab 3/Lab 3 - submission/DUT/aux_package.vhd
@@ -5,8 +5,7 @@ package aux_package is
 -----------------------------------------------------------------
 component control_fsm is
 	generic(Awidth 	  : integer := 16;
-			OPsize    : integer := 4
-			);
+			OPsize    : integer := 4);
 	port   (clk, ena, rst 	: IN std_logic;
 	--input status signals from datapath
 			mov,done_DTCM,and_op,or_op,xor_op : IN std_logic;
@@ -17,18 +16,16 @@ component control_fsm is
 			OPC 			: OUT std_logic_vector (3 downto 0);
 	--output TB control signal to specify program has ended
 			done_TB   			: OUT std_logic
-			
-			
 			);
 end component;
 -----------------------------------------------------------------
-	component Adder IS
+component Adder IS
 		GENERIC (length : INTEGER := 8);
 		PORT ( a, b: IN STD_LOGIC_VECTOR (length-1 DOWNTO 0);
 			cin: IN STD_LOGIC;
             s: OUT STD_LOGIC_VECTOR (length-1 DOWNTO 0);
 			cout: OUT STD_LOGIC);
-	END component;
+END component;
 -----------------------------------------------------------------
 component dataMem is
 generic( Dwidth: integer:=16;
@@ -38,8 +35,7 @@ port(	clk,memEn: in std_logic;
 		WmemData:	in std_logic_vector(Dwidth-1 downto 0);
 		WmemAddr,RmemAddr:	
 					in std_logic_vector(Awidth-1 downto 0);
-		RmemData: 	out std_logic_vector(Dwidth-1 downto 0)
-);
+		RmemData: 	out std_logic_vector(Dwidth-1 downto 0));
 end component;
 -----------------------------------------------------------------
 component BidirPin is
@@ -47,26 +43,114 @@ component BidirPin is
 	port(   Dout: 	in 		std_logic_vector(width-1 downto 0);
 			en:		in 		std_logic;
 			Din:	out		std_logic_vector(width-1 downto 0);
-			IOpin: 	inout 	std_logic_vector(width-1 downto 0)
-	);
+			IOpin: 	inout 	std_logic_vector(width-1 downto 0));
 end component;
 -----------------------------------------------------------------
 component ALU_UNIT IS
   GENERIC (BusSize : INTEGER := 16);
   PORT ( A, B          : IN STD_LOGIC_VECTOR (BusSize-1 DOWNTO 0);
          OPC           : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-         CFlag, Zflag, Nflag : OUT STD_LOGIC;
+         CFlag, Zflag, Nflag : OUT STD_LOGIC := '0';
          C             : OUT STD_LOGIC_VECTOR(BusSize-1 DOWNTO 0);
          Ain           : IN STD_LOGIC;
-         COUT          : IN STD_LOGIC);
+         Cout          : IN STD_LOGIC;
+		 clk		   : in std_logic;
+		 Cin           : in std_logic);
 END component;
 -----------------------------------------------------------------
+component IR IS
+  generic (
+    bus_size : integer := 16
+  );
+  port (
+    bus_in : in std_logic_vector(bus_size-1 downto 0);
+    IRin : in std_logic;
+    RFaddr : in std_logic_vector(1 downto 0);
+    reg_address : out std_logic_vector(3 downto 0);
+    Op : out std_logic_vector(3 downto 0);
+    Imm1 : out std_logic_vector(bus_size-1 downto 0);
+    Imm2 : out std_logic_vector(bus_size-1 downto 0);
+    offset_addr : out std_logic_vector(7 downto 0)
+  );
+END component;
+----------------------------------------------------------------
+component RF IS
+generic( Dwidth: integer:=16;
+		 Awidth: integer:=4);
+port(	clk,rst,WregEn: in std_logic;	
+		WregData:	in std_logic_vector(Dwidth-1 downto 0);
+		WregAddr,RregAddr:	
+					in std_logic_vector(Awidth-1 downto 0);
+		RregData: 	out std_logic_vector(Dwidth-1 downto 0));
+END component;
+----------------------------------------------------------------
+component OPCdecode IS
+	generic(RFAddrWidth: integer:=4); 	-- Register Size
+	port(	IRopBits	:IN std_logic_vector (RFAddrWidth - 1 downto 0);
+			mov, done_DTCM, and_op, or_op, xor_op : OUT std_logic;
+			jnc, jc, jmp, sub, add, ld, st : OUT std_logic);
+END component;
+----------------------------------------------------------------
+component progMem IS
+	generic( Dwidth: integer:=16;
+			 Awidth: integer:=6;
+			 dept:   integer:=64);
+	port(	clk,memEn: in std_logic;	
+			WmemData:	in std_logic_vector(Dwidth-1 downto 0);
+			WmemAddr,RmemAddr:	
+						in std_logic_vector(Awidth-1 downto 0);
+			RmemData: 	out std_logic_vector(Dwidth-1 downto 0));
+END component;
+----------------------------------------------------------------
+component PC_wrap IS
+	GENERIC(Awidth			:INTEGER := 6;
+			OffsetWidth		:INTEGER := 8);
+	PORT(	clk 			:IN std_logic;
+			IRoffset 		:IN std_logic_vector(OffsetWidth-1 DOWNTO 0);
+			PCout			:OUT std_logic_vector(Awidth-1 downto 0) := "000000";
+--control signals
+			PCin     		:IN std_logic;
+			PCsel			:IN std_logic_vector(1 DOWNTO 0));
+END component;
+----------------------------------------------------------------
+component datapath is
+generic( Dwidth: integer:=16;	-- Bus Size
+		 RFAddrWidth: integer:=4; 	-- Register Size
+		 m: 	  integer:=16;  -- Program Memory In Data Size
+		 Awidth:  integer:=6;  	-- Address Size
+		 OffsetSize 	: integer := 8;
+		 ImmidSize	: integer := 8;		 
+		 dept:    integer:=64;
+		 prog_data_size : integer := 16;
+		 prog_addr_size : integer := 6
+		); 
+	port(clk, rst: in std_logic;
+		-- inputs from control unit
+		Mem_wr, Mem_out, Mem_in, Cout, Cin, Ain, RFin, RFout, IRin, PCin, Imm1_in, Imm2_in : in std_logic;
+		OPC : in std_logic_vector(3 downto 0);
+		RFaddr, PCsel : in std_logic_vector(1 downto 0);
+		 -- inputs from tb
+		 -- PROGRAM
+		Prog_wren : in std_logic; -- enable bit
+		ProgMem_Data_in : in std_logic_vector(prog_data_size-1 downto 0);
+		ProgMem_writeAddr : in std_logic_vector(prog_addr_size-1 downto 0);
+		-- DATA
+		TB_Data_wren, TBactive: in std_logic := '0';
+		TB_DataMem_Data_in: in std_logic_vector(m-1 downto 0);
+		TB_Data_writeAddr, TB_Data_readAddr : in std_logic_vector(prog_addr_size-1 downto 0);
+		
+		 -- outputs to control unit
+		mov, done, and_bit, or_bit, xor_bit, jnc, jc, jmp, sub, add, ld, st : out std_logic; -- out from OPCdecoder
+		Nflag, Zflag, Cflag : out std_logic := '0'; -- out from ALU
+		-- outputs to tb, DATA
+		DataMem_Data_out : out std_logic_vector(m-1 downto 0));
+END component;
+----------------------------------------------------------------		
 component datamem_wrap is
 generic( 
 		Dwidth: integer:=16;
 		Awidth: integer:=6;
-		dept:   integer:=64
-		);
+		dept:   integer:=64);
     port(
 --DUT signals		
 		clk, rst :in std_logic;	
@@ -78,22 +162,7 @@ generic(
 		TBactive, TB_wren: in std_logic;
 		TB_wAddr, TB_rAddr	: in std_logic_vector(Awidth-1 downto 0);
 		TB_wData 			: in std_logic_vector(Dwidth-1 downto 0);
-		TB_rData  			: out std_logic_vector(Dwidth-1 downto 0)
-	);	
-		
-	
+		TB_rData  			: out std_logic_vector(Dwidth-1 downto 0));
 end component;
 -----------------------------------------------------------------
-
------------------------------------------------------------------
-
------------------------------------------------------------------
-  
------------------------------------------------------------------
-  
------------------------------------------------------------------
-  
------------------------------------------------------------------
-  
 end aux_package;
-
