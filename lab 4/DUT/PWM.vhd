ec50101d41a6d435e4559b4ee25a12ad3b162393
Hadardoronn <hadardoron5@gmail.com>
2024-07-09 15:17:50 +0300

    dd

--- lab 4/DUT/PWM.vhd
+++ lab 4/DUT/PWM.vhd
@@ -1,7 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+USE ieee.std_logic_arith.all;
+USE ieee.std_logic_unsigned.all;
 USE work.aux_package.all;
 
 ENTITY PWM IS
@@ -17,15 +17,13 @@ ENTITY PWM IS
   );
 END ENTITY PWM;
 
-ARCHITECTURE PWM_MOUDLE OF PWM IS
+ARCHITECTURE PWM_MODULE OF PWM IS
     subtype vector IS STD_LOGIC_vector(n-1 DOWNTO 0);   
     SIGNAL X_PWM: vector;
     SIGNAL Y_PWM: vector;
     SIGNAL COUNTER_PWM: std_logic_vector(n-1 downto 0);
-    SIGNAL PWM_0: STD_LOGIC := '0'; -- IF ALUFN IS 00000, WE WILL GET OUT THIS SIGNAL
-    SIGNAL PWM_1: STD_LOGIC := '0'; -- IF ALUFN IS 00001, WE WILL GET OUT THIS SIGNAL, WHICH IS NOT(PWM_0)
+    SIGNAL PWM_OUT_REG: std_logic := '0';
 BEGIN
-    -- Gate inputs to PWM module when active
     Y_PWM <= Y_PWM_in when ALUFN(4 DOWNTO 3) = "00" else (others => '0');
     X_PWM <= X_PWM_in when ALUFN(4 DOWNTO 3) = "00" else (others => '0');
 
@@ -33,42 +31,34 @@ BEGIN
     begin
         if RST = '1' then
             COUNTER_PWM <= (others => '0');
-            PWM_OUT     <= '0';
+            PWM_OUT_REG <= '0';
         elsif rising_edge(CLK) then
             if ENA = '1' then
-                if (COUNTER_PWM = Y_PWM) then
-                    -- Counter counts only up to Y
+                if COUNTER_PWM = Y_PWM then
                     COUNTER_PWM <= (others => '0');
                 else
                     COUNTER_PWM <= COUNTER_PWM + 1;
                 end if;
-                
-                if COUNTER_PWM >= X_PWM then
-                    if COUNTER_PWM < Y_PWM then    
-                        PWM_0 <= '1';
+
+                if ALUFN = "00000" then
+                    if (COUNTER_PWM > X_PWM) and (COUNTER_PWM < Y_PWM) then
+                        PWM_OUT_REG <= '1';
                     else
-                        PWM_0 <= '0';
+                        PWM_OUT_REG <= '0';
                     end if;
-                else
-                    PWM_0 <= '0';
+                elsif ALUFN = "00001" then
+                    if (COUNTER_PWM > X_PWM) and (COUNTER_PWM < Y_PWM) then
+                        PWM_OUT_REG <= '0';
+                    else
+                        PWM_OUT_REG <= '1';
+                    end if;
                 end if;
             else
-                PWM_0 <= '0';
+                PWM_OUT_REG <= '0';
             end if;
         end if;
     end process;
 
-    PWM_1 <= not PWM_0;
+    PWM_OUT <= PWM_OUT_REG;
 
-    process(ALUFN, PWM_0, PWM_1)
-    begin
-        if ALUFN = "00000" then
-            PWM_OUT <= PWM_0;
-        elsif ALUFN = "00001" then
-            PWM_OUT <= PWM_1;
-        else 
-            PWM_OUT <= '0';
-        end if;
-    end process;
-
-END ARCHITECTURE PWM_MOUDLE;
+END ARCHITECTURE PWM_MODULE;
