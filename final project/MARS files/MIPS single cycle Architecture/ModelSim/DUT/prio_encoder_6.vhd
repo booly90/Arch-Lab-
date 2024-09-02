5dd68e20258b1f63cf87936ccd7c9e01a7c8553d
Hadardoronn <hadardoron5@gmail.com>
2024-09-02 17:30:59 +0300

    ff

--- final project/MARS files/MIPS single cycle Architecture/ModelSim/DUT/prio_encoder_6.vhd
+++ final project/MARS files/MIPS single cycle Architecture/ModelSim/DUT/prio_encoder_6.vhd
@@ -13,14 +13,14 @@ ENTITY priotity_encoder IS
 END priotity_encoder;
 ----------------------------
 ARCHITECTURE structure OF priotity_encoder IS
-
+signal	counter : STD_LOGIC_VECTOR( 1 DOWNTO 0 );
 BEGIN
 
-	type_signal  <= 	'0000' 	WHEN INT_reqs(0) = '1' ELSE -- type = X"00" 
+	type_signal  <= 	'000000' 	WHEN INT_reqs(0) = '1' ELSE -- type = X"00" 
 				'010000' 	WHEN INT_reqs(1) = '1' ELSE 		-- type = X"10"
 				'010100' 	WHEN INT_reqs(2) = '1' ELSE 		-- type = X"14"
 				'011000' 	WHEN INT_reqs(3) = '1' ELSE 		-- type = X"18"
 				'011100' 	WHEN INT_reqs(4) = '1' ELSE 		-- type = X"1c"
 				'100000' 	WHEN INT_reqs(5) = '1' ELSE 		-- type = X"20" 
-				'000000'                              
+				'000000'   			
 END ARCHITECTURE;                                   
